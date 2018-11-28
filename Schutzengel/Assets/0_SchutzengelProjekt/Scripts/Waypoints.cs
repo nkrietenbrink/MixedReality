350f037990a22f15fc3e2c98417728f26b2f2aeb
Nadine Krietenbrink <n.krietenbrink@gmail.com>
2018-11-28 23:34:37 +0100

    Erweiterung Town

---
+++ Schutzengel/Assets/0_SchutzengelProjekt/Scripts/Waypoints.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour {
+
+    public GameObject[] waypoints;
+    int current = 0;
+    float rotSpeed;
+    public float speed;
+    public float[] rotations;
+    public bool repeat;
+    public float WPradius;
+    public bool isAnimating;
+    public GameObject fireCar;
+    private void Start()
+    {
+        fireCar = GameObject.Find("fireCar");
+    }
+    void Update()
+    {
+        if(isAnimating) {
+
+            Debug.Log("fireCar.transform.position:  " + fireCar.transform.position);
+            Debug.Log("transform.position:  " + transform.position);
+
+            if (Vector3.Distance(fireCar.transform.position, transform.position) < 10){
+            } else {
+                isAnimating = true;
+                transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
+                if (Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius)
+                {
+                    transform.Rotate(0, rotations[current], 0);
+                    current++;
+
+                        if  (current >= waypoints.Length)
+                    {
+                            if(repeat) {
+                                current = 0;
+                            
+                        } else {
+                            isAnimating = false;
+                        }
+                        }
+                                
+                }
+
+        }
+
+
+    }
+    }
+}
