0ed4e9b9f7f20a3b8b488704cf721c0f25a2f930
Webentwicklung "Webblue <mail@webblue.eu>
2018-11-29 00:29:05 +0100

    More Walks
    WALKING!

--- Schutzengel/Assets/1SchutzengelProjekt/Scripts/Waypoints.cs
+++ Schutzengel/Assets/1SchutzengelProjekt/Scripts/Waypoints.cs
@@ -10,6 +10,8 @@ public class Waypoints : MonoBehaviour {
     public float speed;
     public float[] rotations;
     public bool repeat;
+    public bool backward;
+    private bool isBackward;
     public float WPradius;
     public bool isAnimating;
 
@@ -19,25 +21,51 @@ public class Waypoints : MonoBehaviour {
             transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
             if (Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius)
             {
-
-                transform.Rotate(0, rotations[current], 0);
-                    current++;
-
-                    if  (current >= waypoints.Length)
+                
+                if (!isBackward)
                 {
-                        if(repeat) {
+                    if (rotations.Length > 0) transform.Rotate(0, rotations[current], 0);
+                    current++;
+                    if (current >= waypoints.Length)
+                    {
+                        if (backward)
+                        {
+                            transform.Rotate(0, 180, 0);
+                            isBackward = true;
+                            current-=2;
+                        }
+                        else if (repeat)
+                        {
                             current = 0;
-                        
-                    } else {
-                        isAnimating = false;
+                        }
+                        else
+                        {
+                            isAnimating = false;
+                        }
+                    } else { 
+}
+                }
+                else
+                {
+                    if (rotations.Length > 0 && rotations[current] != 0) transform.Rotate(0, rotations[current] + 180, 0);
+                    if (current == 0)
+                    {
+                        if (repeat)
+                        {
+                            current = 0;
+                            transform.Rotate(0, 180, 0);
+                            isBackward = false;
+                        }
+                        else
+                        {
+                            isAnimating = false;
+                        }
+                    } else
+                    {
+                        current--;
                     }
-                    }
-                            
+                }
             }
-        
-       
-
-
         }
     }
 }
